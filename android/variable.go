a7981c69825b86d2507e8bf0f033b354e0d50d02
AnkitGourav <Ankitgourav035@gmail.com>
2019-01-09 18:04:15 +0530

    ionize

--- android/variable.go
+++ android/variable.go
@@ -20,7 +20,7 @@ import (
 	"runtime"
 	"strings"
 
-	"aosp/soong/android"
+	"ion/soong/android"
 
 	"github.com/google/blueprint/proptools"
 )
@@ -117,8 +117,8 @@ type variableProperties struct {
 			Srcs         []string
 		}
 
-		// include custom variables
-		Aosp android.Product_variables
+		// include ion variables
+		Ion android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -236,8 +236,8 @@ type productVariables struct {
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
 
-	// include Custom variables
-	Aosp android.ProductVariables
+	// include Ion variables
+	Ion android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
